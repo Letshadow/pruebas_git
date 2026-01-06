e9210a01e035823ecdcec6642c29fe4c322ad1fd
U-HILDEBOG\rojaswy <rojaswy@HBOG1CZ14601N9.HILDEBOG.LOCAL>
2026-01-06 16:07:39 -0500

    se detecto un bug

--- hi.py
+++ hi.py
@@ -15,6 +15,8 @@ print("En algun algun lugar de ortezal, 3 ingenieros estaban programando")
 #
 # Inventa un bug
 #
+print("se detecto un error al momento de ir al gym ya que Joseth se quedo dormido")
+#
 # FABIO
 #
 # llego tarde a ver que paso
