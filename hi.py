eb446b4498de7ef7cb8e605e0bca5a26e2440e85
davidadran <davidricardoluquelc@gmail.com>
2026-01-06 15:39:06 -0500

    reparto

--- hi.py
+++ hi.py
@@ -1,1 +1,21 @@
 print("Hola Mundo")
+# REPARTO :)
+#
+print("En algun algun lugar de ortezal, 3 ingenieros estaban programando")
+#
+# SEBAS
+#
+# Desayuno
+#
+# JOSETH
+#
+# Tareas del dia
+#
+# WENDY
+#
+# Inventa un bug
+#
+# FABIO
+#
+# llego tarde a ver que paso
+#
